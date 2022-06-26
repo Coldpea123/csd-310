c17dbdad9c0b0eaade1495e4a6bbfd180a1354af
Coldpea123 <102326609+Coldpea123@users.noreply.github.com>
2022-06-26 00:32:01 -0700

    Update pytech_update.py

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -1,1 +1,15 @@
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.lqlwz.mongodb.net/?retryWrites=true&w=majority";
+client = MongoClient(url)
+db = client.pytech
 
+i4 = db.students.find({},{"_id": 0})
+
+print("-- DISPLAYING STUDENTS FROM DOCUMENTS FROM find() QUERY --")
+for doc1 in i4:
+    print(doc1)
+
+result = db.students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Carter"}})
+
+print("-- DISPLAYING UPDATED STUDENT DOCUMENT FROM find_one() QUERY --")
+print(db.students.find_one({"student_id": "1007"}, {"_id": 0}))
